82214982bd7b679abdaaa6fe8fbfd21a5c777fb8
Doug Palmer <dpalmer@chicagobooth.edu>
2017-05-29 20:27:46 -0500

    updates

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,69 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    render("users/index.html.erb")
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    render("users/show.html.erb")
+  end
+
+  def new
+    @user = User.new
+
+    render("users/new.html.erb")
+  end
+
+  def create
+    @user = User.new
+
+    @user.email = params[:email]
+    @user.username = params[:username]
+    @user.password = params[:password]
+
+    save_status = @user.save
+
+    if save_status == true
+      redirect_to("/users/#{@user.id}", :notice => "User created successfully.")
+    else
+      render("users/new.html.erb")
+    end
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+
+    render("photos/edit.html.erb")
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    @photo.caption = params[:caption]
+    @photo.image = params[:image]
+    @photo.user_id = params[:user_id]
+
+    save_status = @photo.save
+
+    if save_status == true
+      redirect_to("/photos/#{@photo.id}", :notice => "Photo updated successfully.")
+    else
+      render("photos/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+
+    @photo.destroy
+
+    if URI(request.referer).path == "/photos/#{@photo.id}"
+      redirect_to("/", :notice => "Photo deleted.")
+    else
+      redirect_to(:back, :notice => "Photo deleted.")
+    end
+  end
+end
