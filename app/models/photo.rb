69ce08385d816ae01401e8e6efaf0dec09b83aca
Doug Palmer <dpalmer@chicagobooth.edu>
2017-06-03 14:24:04 -0500

    updates

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,10 +4,10 @@ class Photo < ApplicationRecord
   belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
 
   # a photo has many comments
-  has_many(:comments, :class_name => "Comment", :foreign_key => "comment_id", :dependent => :destroy)
+  has_many(:comments, :class_name => "Comment", :foreign_key => "comment_id")
 
   # a photo has many likes
-  has_many(:likes, :class_name => "Like", :foreign_key => "like_id", :dependent => :destroy)
+  has_many(:likes, :class_name => "Like", :foreign_key => "like_id")
 
   # a photo has many fans who like the photo
   has_many :fans, :through => :likes, :source => :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :trackable, :validatable
 
   # a user has many photos
-  has_many(:photos, :class_name => "Photo", :foreign_key => "user_id", :dependent => :destroy)
+  has_many(:photos, :class_name => "Photo", :foreign_key => "user_id")
 
   # a user has many comments
-  has_many(:comments, :class_name => "Comment", :foreign_key => "comment_id", :dependent => :destroy)
+  has_many(:comments, :class_name => "Comment", :foreign_key => "comment_id")
 
   # a user has many likes
-  has_many(:likes, :class_name => "Like", :foreign_key => "like_id", :dependent => :destroy)
+  has_many(:likes, :class_name => "Like", :foreign_key => "like_id")
 
   # a user has many liked photos
   has_many :liked_photos, :through => :likes, :source => :photo
